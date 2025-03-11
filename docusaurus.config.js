069281ba142f96357fc4728bc9204cfccdf21771
CapellaDocuments <188106524+CapellaDocuments@users.noreply.github.com>
2025-03-11 15:07:57 -0700

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,15 +15,15 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://CapellaDocuments.github.io',
+  url: 'https://CapellaSystems.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/CapellaDocumentsTest/',
+  baseUrl: '/CapellaSystems/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'CapellaDocuments', // Usually your GitHub org/user name.
-  projectName: 'CapellaDocumentsTest', // Usually your repo name.
+  organizationName: 'CapellaSystems', // Usually your GitHub org/user name.
+  projectName: 'docs', // Usually your repo name.
   deploymentBranch: 'gh-pages',
   
   onBrokenLinks: 'warn',
