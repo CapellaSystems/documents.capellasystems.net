a41710e637ee2a63b0a561f6f1f1560ff576e070
unknown <build@prodeski5-sff.hq.capellasystems.net>
2025-10-01 14:32:29 -0700

    Commit on Wed 10/01/2025 14:32:28.92

--- sidebars.js
+++ sidebars.js
@@ -223,8 +223,8 @@ const sidebars = {
         'Cambria FTC/Tutorials/Subtitle_Burn-In/subtitle-burnin',
         'Cambria FTC/Tutorials/scte-35-insertion',
         'Cambria FTC/Tutorials/Snmp_Setup_and_testing/snmp-guide',
-        'Cambria FTC/Tutorials/Using the Teletrax Watermarking Filter/teletrax-watermarking',
-        'Cambria FTC/Tutorials/Video Segments/video-segments',
+        'Cambria FTC/Tutorials/Using_the_Teletrax_Watermarking_Filter/teletrax-watermarking',
+        'Cambria FTC/Tutorials/Video_Segments/video-segments',
         'Cambria FTC/Tutorials/XML_Titler_with_Carbon_Subtitle_Workflow/carbon-subtitle-xml'
       ],
     },
